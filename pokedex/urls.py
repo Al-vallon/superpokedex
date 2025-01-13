cfa58e812afd7a6172b548c0e72c30821b94c691
Sylvain Conan <sylvain.mj.conan@gmail.com>
2025-01-13 16:45:33 +0100

    search bar & autocomplete

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('pokemons', views.getAllPokemon, name='getAllPokemon'), 
     path('pokemons/<int:id>', views.getPokemonById, name='getPokemonById'),
     path('search_pokemon/', views.searchPokemon, name='search_pokemon'),
-    # path('teams', teamsview.manageTeam, name='manageTeam'),
 ]
