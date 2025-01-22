5e0713d1a71ed6470115b079d1ba621993f78079
Sylvain Conan <sylvain.mj.conan@gmail.com>
2025-01-22 18:33:51 +0100

    edit pokemons of a team, + fix the buggy css, + optimized PUT & DELETE requests

--- pokedex/teamsview.py
+++ pokedex/teamsview.py
@@ -13,31 +13,35 @@ from pokedex.views import API_URL
 @require_http_methods(["GET", "POST", "PUT", "PATCH", "DELETE"])
 def manageTeam(request):
 
-    try:
-        response = requests.get(f"{API_URL}?&limit=151")
-        response.raise_for_status()
-        data = response.json()
-    except requests.exceptions.RequestException:
-        return HttpResponse("Pokémons not found", status=404)
-    pokemons = data.get("results", [])
-
-    all_pokemons = []
-    for pokemon in pokemons:
-        pokemon_url = pokemon['url']
+    if request.method in {'GET', 'POST'}:
         try:
-            response_pokemon = requests.get(pokemon_url)
-            response_pokemon.raise_for_status()
-            data_pokemon = response_pokemon.json()
-            name = data_pokemon['name']
-            image_url = data_pokemon['sprites']['front_default']
-            all_pokemons.append({
-                'id' : data_pokemon['id'],
-                'name': name,
-                'image_url': image_url,
-            })
+            response = requests.get(f"{API_URL}?&limit=151")
+            response.raise_for_status()
+            data = response.json()
         except requests.exceptions.RequestException:
-            continue  
+            return HttpResponse("Pokémons not found", status=404)
+        pokemons = data.get("results", [])
+
         teams = Teams.objects.all()
+        for team in teams:
+            team.pokemons = [team.pokemon_1,team.pokemon_2,team.pokemon_3,team.pokemon_4,team.pokemon_5,team.pokemon_6]
+        
+        all_pokemons = []
+        for pokemon in pokemons:
+            pokemon_url = pokemon['url']
+            try:
+                response_pokemon = requests.get(pokemon_url)
+                response_pokemon.raise_for_status()
+                data_pokemon = response_pokemon.json()
+                name = data_pokemon['name']
+                image_url = data_pokemon['sprites']['front_default']
+                all_pokemons.append({
+                    'id' : data_pokemon['id'],
+                    'name': name,
+                    'image_url': image_url,
+                })
+            except requests.exceptions.RequestException:
+                continue  
 
     if request.method == 'GET':
         return render(request, 'teams.html', {'teams': teams, 'all_pokemons': all_pokemons})
@@ -98,15 +102,27 @@ def manageTeam(request):
             data = json.loads(request.body)
             team_id = data.get('team_id')
             new_name = data.get('name')
+            pokemon_1 = data.get('pokemon_1')
+            pokemon_2 = data.get('pokemon_2')
+            pokemon_3 = data.get('pokemon_3')
+            pokemon_4 = data.get('pokemon_4')
+            pokemon_5 = data.get('pokemon_5')
+            pokemon_6 = data.get('pokemon_6')
             # print(f'team_id', team_id), print(f'new_name', new_name)
 
             if team_id and new_name:
                 team = Teams.objects.get(id=team_id)
                 team.name = new_name
+                team.pokemon_1 = pokemon_1
+                team.pokemon_2 = pokemon_2
+                team.pokemon_3 = pokemon_3
+                team.pokemon_4 = pokemon_4
+                team.pokemon_5 = pokemon_5
+                team.pokemon_6 = pokemon_6
                 team.save()
-                return JsonResponse({'message': f"Team '{team_id}' updated to '{new_name}' successfully!"}, status=200)
+                return JsonResponse({'message': f"Team '{team_id}' updated successfully!"}, status=200)
             else:
-                return JsonResponse({'message': "Team ID and new name are required."}, status=400)
+                return JsonResponse({'message': "Team ID and name are required."}, status=400)
         except json.JSONDecodeError:
             return JsonResponse({'message': "Invalid JSON data."}, status=400)
         except Teams.DoesNotExist:
