0f98607f49d6dd336109d10ab33f86063056cda3
basile <basilezic@gmail.com>
2025-01-23 20:28:01 +0100

    Feat : add combat system

--- pokedex/views.py
+++ pokedex/views.py
@@ -56,12 +56,41 @@ def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
 def searchPokemon(request):
-    pokemon_name = request.GET.get("pokemon-name", "").strip()
-    url = f"{API_URL}{pokemon_name}"
-    response = requests.get(url)
-    if response.status_code == 200:
+    pokemon_name = request.GET.get("pokemon-name", "").strip().lower()
+    if not pokemon_name:
+        return HttpResponse("Pokémon name is required", status=400)
+    
+    try:
+        response = requests.get(f"{API_URL}?limit=151")
+        response.raise_for_status()
         data = response.json()
-        print(f"data: {data}")
-        return render(request, "pokemon.html", {"pokemon": data})
-    else:
-        return HttpResponse("Pokémon not found", status=404)
+    except requests.exceptions.RequestException:
+        return HttpResponse("Pokémons not found", status=404)
+    
+    matching_pokemons = [pokemon for pokemon in data.get("results", []) if pokemon_name in pokemon['name']]
+    
+    if not matching_pokemons:
+        return HttpResponse("No matching Pokémon found", status=404)
+    
+    all_pokemons = []
+    for pokemon in matching_pokemons:
+        pokemon_url = pokemon['url']
+        try:
+            response_pokemon = requests.get(pokemon_url)
+            response_pokemon.raise_for_status()
+            data_pokemon = response_pokemon.json()
+            name = data_pokemon['name']
+            image_url = data_pokemon['sprites']['front_default']
+            all_pokemons.append({
+                'name': name,
+                'image_url': image_url,
+            })
+        except requests.exceptions.RequestException:
+            continue
+    
+    return render(request, "pokedex.html", {
+        'all_pokemons': all_pokemons,
+        "next_offset": None,
+        "previous_offset": None,
+        "current_offset": 0,
+    })
